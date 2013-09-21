45db9f39dda0f39caef962800244ff7830d62cf1
yangqibin <pipi32167@gmail.com>
2013-09-21 09:08:33 +0800

    init

---
+++ DoubleScreenMatchIt/DoubleScreenMatchIt/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  DoubleScreenMatchIt
+//
+//  Created by yangqibin on 13-9-20.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
